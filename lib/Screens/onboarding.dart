b0e59ad3e8a3df38e8e11f1800e719161913e902
DELL <mahalakshmi@capztone.com>
2024-09-19 11:23:16 +0530

    change

--- lib/Screens/onboarding.dart
+++ lib/Screens/onboarding.dart
@@ -70,7 +70,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 imagePath: 'assets/images/onboarding3.png',
                 title: 'Discover Experienced Doctors',
                 description:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel felis nec magna consequat tincidunt.',
+                    'Lorem ipsum dolor sit , consectetur adipiscing elit. Donec vel felis nec magna consequat tincidunt.',
               ),
             ],
           ),
